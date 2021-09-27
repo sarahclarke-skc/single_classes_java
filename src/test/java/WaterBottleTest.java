fe8f2849d1527be081fb98f0a5bfc8f65d32926e
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-09-27 14:19:54 +0100

    Water Bottle tests running

--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -1,2 +1,35 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class WaterBottleTest {
+
+    WaterBottle waterBottle;
+
+    @Before
+    public void before() {
+        waterBottle = new WaterBottle(100);
+    }
+
+    @Test
+    public void hasVolume() {
+        assertEquals(100, waterBottle.getVolume());
+    }
+
+    @Test
+    public void canDrink() {
+        assertEquals(90, waterBottle.drink());
+    }
+
+    @Test
+    public void canEmpty() {
+        assertEquals(0, waterBottle.empty());
+    }
+
+    @Test
+    public void canFill() {
+        assertEquals(100, waterBottle.fill());
+    }
+
 }
